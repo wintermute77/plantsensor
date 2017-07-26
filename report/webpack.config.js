a3cbb8545557090ce6947db94bab2aa501445ff3
Ben Casey <ben.casey@havasww.com>
2017-07-26 16:52:27 +0100

    Added report page

---
+++ report/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist/assets'),
+    publicPath: '/assets',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+};
